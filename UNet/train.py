06cd29b0b99615d1500e30d475f22de5e0a6b1db
ToThanhDat <tothanhdat1421@gmail.com>
2025-01-26 18:05:20 +0700

    Complete load_data.py and first half unet.py

--- UNet/train.py
+++ UNet/train.py
@@ -1,6 +1,66 @@
+import logging
+import tqdm
+import wandb
+import matplotlib.pyplot as plt
+
+import os
+import numpy as np
 import torch
 import torch.nn as nn
+from torch.utils.data import random_split, DataLoader
+from pathlib import Path
+from unet.unet import UNET
+from utils.load_data import ImageCustomDataset, MaskCustomDataset
+
+dir_images = Path('./data/imgs')
+dir_masks = Path('./data/masks')
+dir_checkpoint = Path('./checkpoints/')
+
+def train_model(
+        model, 
+        device, 
+        n_epochs: int = 5,
+        batch_size: int = 1,
+        learning_rate: float = 1e-5,
+        val_percent: float = 0.,
+        save_checkpoint: bool = True,
+        img_scale: float = 0.5,
+        amp: bool = True,
+        weight_decay: float = 1e-8,
+        momentum: float = 0.99,
+        gradient_clipping: float = 1.0,
+):
+    # 1. Create dataset
+    dataset = MaskCustomDataset(dir_images, dir_masks, img_scale)
+
+    # tmp_img = dataset[0]['image'].permute(1, 2, 0)
+    # plt.imshow(np.asarray(tmp_img))
+    # plt.show()
+
+    # 2. Split into train / validation partitions
+    n_val = int(val_percent * len(dataset))
+    n_train = len(dataset) - n_val
+    train_set, test_set = random_split(dataset, [n_train, n_val], generator = torch.Generator().manual_seed(86))
+
+    # 3. Create data loaders
+    loader_args = dict(batch_size=batch_size, num_workers=os.cpu.count(), pin_memory=True)
+    train_dataloader = DataLoader(train_set, shuffle=True, **loader_args)
+    test_dataloader = DataLoader(test_set, shuffle=True, **loader_args)
+
+    # 4. Set up the optimizer, the loss, the learning rate scheduler and the loss scaling for AMP
+    
+    # 5. Begin training
+    return
 
 if __name__ == "__main__":
-    with torch.no_grad():
-        print("Training...")
+    print("Training...")
+
+    model = UNET()
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    logging.info(f'Using device: {device}')
+    train_model(
+        model,
+        device,
+    )
+    
--- UNet/unet/unet.py
+++ UNet/unet/unet.py
@@ -5,12 +5,32 @@ import torch.nn.functional as F
 class UNET(nn.Module):
     def __init__(self) -> None:
         super(UNET, self).__init__()
+        self.maxpool = nn.MaxPool2d(2, 1)
 
-    def contracting_path(self):
-        return
+    def contracting_path(self, X):
+        # 572 x 572
+        out1 = self.down1(X)
+        X = self.maxpool(out1) # 568 x 568
+        # 284 x 284
+        out2 = self.down2(X)
+        X = self.maxpool(out2) # 280 x 280
+        # 140 x 140
+        out3 = self.down3(X)
+        X = self.maxpool(out3) # 136 x 136
+        # 68 x 68
+        out4 = self.down4(X)
+        X = self.maxpool(out4) # 64 x 64
+        return out1, out2, out3, out4, X
     
-    def expansive_path(self):
+    def expansive_path(self, X, c4, c3, c2, c1):
+        
         return
     
     def forward(self, X):
+        c1, c2, c3, c4, X = self.contracting_path(X) # 64 x 64
+        X = self.maxpool(X) # 32 x 32
+        for _ in range(2):
+            X = nn.Conv2d(512, 1024, kernel_size=3, stride=1)(X)
+        # X: 28 x 28
+        X = self.expansive_path(X, c4, c3, c2, c1)
         return X
